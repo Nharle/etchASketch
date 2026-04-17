2f5b641ec7266c85120d24b75c5dac2e5bae61bc
Harry Le <88409590+Nharle@users.noreply.github.com>
2026-04-17 08:10:00 -0700

    Made the sketchpad color in boxes when the mouse clicks and drags inside of it

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
+/*
+IF mouseDown make clicked  
+Change color of the div  
+ */
 let row = 16;
 let collumn = 16;
 let container = document.querySelector(".container");
+let color = "#000000";
+let drawing = false;
+
+function colorItem(e) {
+    if(drawing) {
+        console.log("hello");
+        e.target.style.backgroundColor = color;
+        
+    }
+}
 
 for(let i =0; i < row; i++){
     let gridRow = document.createElement("div");
@@ -13,3 +27,17 @@ for(let i =0; i < row; i++){
 
     }
 }
+
+
+
+container.addEventListener("pointerdown", (e) => {
+    drawing=true;
+    e.preventDefault();
+    
+});
+container.addEventListener("pointermove", colorItem);
+window.addEventListener("pointerup", () => {
+    drawing=false;
+    
+});
+
